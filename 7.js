f17148ca7f33c14be2a36930a89dee51c5fcbc03
Manav Misra <manavm@visionwebsoft.com>
2022-10-18 22:09:34 -0500

    Show Logical Operators

---
+++ 7.js
@@ -0,0 +1,13 @@
+const mathGrade = "A";
+const isRoomClean = true;
+
+// TODO: Write an if statement that prints "You get a gold star!" if mathGrade is an A and isRoomClean is true.
+// Logical Operators - AND (&&) and OR (||)
+if (mathGrade === "A" && isRoomClean === true) {
+  console.log("You get a gold star!");
+}
+
+// TODO: Write an if statement that prints "We can get ice cream!" if mathGrade is an A or isRoomClean is true.
+if (mathGrade === "A" || isRoomClean === true) {
+  console.log("We can get ice cream!");
+}
